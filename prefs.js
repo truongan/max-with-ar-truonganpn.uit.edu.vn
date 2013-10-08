494c5af1adae919f119c76ede6bfc9e43cc4f491
pntruongan <pntruongan@gentoopc>
2013-10-09 03:00:47 +0700

    crazy stuff

--- prefs.js
+++ prefs.js
@@ -15,154 +15,110 @@ const Gettext = imports.gettext.domain('gnome-shell-extensions');
 const _ = Gettext.gettext;
 
 let createSlider = function(configName) {
-  let ret = new Gtk.Scale({digits: 0, sensitive: true, orientation: Gtk.Orientation.HORIZONTAL, margin_right: 6, margin_left: 6});
-  ret.set_range(0, 100);
-  ret.set_value(Utils.getNumber(configName, 50));
-  ret.set_value_pos(Gtk.PositionType.RIGHT);
-  ret.connect("value-changed", function(obj, userData) {
-    Utils.setParameter(configName, obj.get_value());
-  });
-  return ret;
+	let ret = new Gtk.Scale({digits: 0, sensitive: true, orientation: Gtk.Orientation.HORIZONTAL, margin_right: 6, margin_left: 6});
+	ret.set_range(0, 100);
+	ret.set_value(Utils.getNumber(configName, 50));
+	ret.set_value_pos(Gtk.PositionType.RIGHT);
+	ret.connect("value-changed", function(obj, userData) {
+		Utils.setParameter(configName, obj.get_value());
+	});
+	return ret;
 }
 
 
-const PutWindowSettingsWidget = new GObject.Class({
-  Name: 'PutWindow.Prefs.PutWindowSettingsWidget',
-  GTypeName: 'PutWindowSettingsWidget',
-  Extends: Gtk.Notebook,
+const temporary_widget_settings = new GObject.Class({
+	Name: 'PutWindow.Prefs.temporary_widget_settings',
+	GTypeName: 'temporary_widget_settings',
+	Extends: Gtk.Notebook,
 
-  _init : function(params) {
-    this.parent(params);
-    this.orientation = Gtk.Orientation.VERTICAL;
-    this.hexpand = true;
-    this._wnckScreen = Wnck.Screen.get_default();
+	_init : function(params) {
+		this.parent(params);
+		this.orientation = Gtk.Orientation.VERTICAL;
+		this.hexpand = true;
+		this._wnckScreen = Wnck.Screen.get_default();
 
-    this.append_page(this._createKeyboardConfig(), new Gtk.Label({label: "<b>Keyboard Shortcuts</b>",
-         halign:Gtk.Align.START, margin_left: 4, use_markup: true}));
+		this.append_page(this._createKeyboardConfig(), new Gtk.Label({label: "<b>Keyboard Shortcuts</b>",
+				 halign:Gtk.Align.START, margin_left: 4, use_markup: true}));
 
-   },
+	 },
 
-  _createCornerChangesCombo: function() {
-    this._model = new Gtk.ListStore();
-    this._model.set_column_types([GObject.TYPE_STRING, GObject.TYPE_STRING]);
 
-    let combo = new Gtk.ComboBox({ model: this._model, halign: Gtk.Align.END});
+	_addSliders: function(grid, row, labels, configName) {
+		for (let i=0; i < labels.length; i++) {
+			row++;
+			grid.attach(new Gtk.Label({label: labels[i], halign:Gtk.Align.START, margin_left: 15 }), 0, row, 1, 1);
+			grid.attach(createSlider(configName + "-" + i), 1, row, 5, 1);
+		}
+		return row;
+	},
 
-    let currentValue = Utils.getNumber(Utils.CORNER_CHANGE, 0);
+	_createKeyboardConfig: function() {
+		return this._createBindingList({
+				"maximize": "Maximize with aspect ratio",
+				"restore": "Restore window",
+		});
+	},
 
-    let values = [
-      ["0", "Both"],
-      ["1", "Only height"],
-      ["2", "Only width"],
-      ["3", "Never change size"],
-      ["4", "Nothing on first move, both on second"],
-      ["5", "Nothing on first move, Only height on second"],
-      ["6", "Nothing on first move, Only width on second"]
-    ]
-    let selectMe = null;
-    for (let i=0; i< values.length; i++) {
-      let iter = this._model.append();
-      this._model.set(iter, [0, 1], values[i]);
-      if (values[i][0] == currentValue) {
-        selectMe = iter;
-      }
-    }
+	_createBindingList: function(bindings) {
 
-    if (selectMe != null) {
-      combo.set_active_iter(selectMe);
-    }
+		let name, model = new Gtk.ListStore();
 
-    let renderer = new Gtk.CellRendererText();
-    combo.pack_start(renderer, true);
-    combo.add_attribute(renderer, 'text', 1);
-    combo.connect("changed", Lang.bind(this,
-      function(obj) {
-        let[success, iter] = obj.get_active_iter();
-        if (!success) {
-          return;
-        }
-        Utils.setParameter(Utils.CORNER_CHANGE, this._model.get_value(iter, 0));
-      })
-    );
-    return combo;
-  },
+		model.set_column_types([
+			GObject.TYPE_STRING,
+			GObject.TYPE_STRING,
+			GObject.TYPE_INT,
+			GObject.TYPE_INT
+		]);
 
-  _addSliders: function(grid, row, labels, configName) {
-    for (let i=0; i < labels.length; i++) {
-      row++;
-      grid.attach(new Gtk.Label({label: labels[i], halign:Gtk.Align.START, margin_left: 15 }), 0, row, 1, 1);
-      grid.attach(createSlider(configName + "-" + i), 1, row, 5, 1);
-    }
-    return row;
-  },
+		for (name in bindings) {
+			let [key, mods] = Gtk.accelerator_parse(Utils.get_strv(name, null)[0]);
+			let row = model.insert(10);
+			model.set(row, [0, 1, 2, 3], [name, bindings[name], mods, key ]);
+		}
 
-  _createKeyboardConfig: function() {
-    return this._createBindingList({
-        "maximize": "Maximize with aspect ratio",
-        "restore": "Restore window",
-    });
-  },
+		let treeview = new Gtk.TreeView({
+			'expand': true,
+			'model': model,
+			margin: 4
+		});
 
-  _createBindingList: function(bindings) {
+		// Action column
+		let cellrend = new Gtk.CellRendererText();
+		let col = new Gtk.TreeViewColumn({ 'title': 'Action', 'expand': true });
+		col.pack_start(cellrend, true);
+		col.add_attribute(cellrend, 'text', 1);
+		treeview.append_column(col);
 
-    let name, model = new Gtk.ListStore();
+		// keybinding column
+		cellrend = new Gtk.CellRendererAccel({
+			'editable': true,
+			'accel-mode': Gtk.CellRendererAccelMode.GTK
+		});
 
-    model.set_column_types([
-      GObject.TYPE_STRING,
-      GObject.TYPE_STRING,
-      GObject.TYPE_INT,
-      GObject.TYPE_INT
-    ]);
+		cellrend.connect('accel-edited', function(rend, iter, key, mods) {
+			let value = Gtk.accelerator_name(key, mods);
+			let [succ, iterator ] = model.get_iter_from_string(iter);
 
-    for (name in bindings) {
-      let [key, mods] = Gtk.accelerator_parse(Utils.get_strv(name, null)[0]);
-      let row = model.insert(10);
-      model.set(row, [0, 1, 2, 3], [name, bindings[name], mods, key ]);
-    }
+			if(!succ) {
+				throw new Error("Error updating Keybinding");
+			}
 
-    let treeview = new Gtk.TreeView({
-      'expand': true,
-      'model': model,
-      margin: 4
-    });
+			let name = model.get_value(iterator, 0);
 
-    // Action column
-    let cellrend = new Gtk.CellRendererText();
-    let col = new Gtk.TreeViewColumn({ 'title': 'Action', 'expand': true });
-    col.pack_start(cellrend, true);
-    col.add_attribute(cellrend, 'text', 1);
-    treeview.append_column(col);
+			model.set(iterator, [ 2, 3], [ mods, key ]);
+			Utils.set_strv(name, [value]);
+		});
 
-    // keybinding column
-    cellrend = new Gtk.CellRendererAccel({
-      'editable': true,
-      'accel-mode': Gtk.CellRendererAccelMode.GTK
-    });
+		col = new Gtk.TreeViewColumn({'title': 'Modify'});
 
-    cellrend.connect('accel-edited', function(rend, iter, key, mods) {
-      let value = Gtk.accelerator_name(key, mods);
-      let [succ, iterator ] = model.get_iter_from_string(iter);
+		col.pack_end(cellrend, false);
+		col.add_attribute(cellrend, 'accel-mods', 2);
+		col.add_attribute(cellrend, 'accel-key', 3);
 
-      if(!succ) {
-        throw new Error("Error updating Keybinding");
-      }
+		treeview.append_column(col);
 
-      let name = model.get_value(iterator, 0);
-
-      model.set(iterator, [ 2, 3], [ mods, key ]);
-      Utils.set_strv(name, [value]);
-    });
-
-    col = new Gtk.TreeViewColumn({'title': 'Modify'});
-
-    col.pack_end(cellrend, false);
-    col.add_attribute(cellrend, 'accel-mods', 2);
-    col.add_attribute(cellrend, 'accel-key', 3);
-
-    treeview.append_column(col);
-
-    return treeview;
-  }
+		return treeview;
+	}
 
 
 });
@@ -172,7 +128,7 @@ function init() {
 }
 
 function buildPrefsWidget() {
-    let widget = new PutWindowSettingsWidget();
-    widget.show_all();
-    return widget;
+	let widget = new temporary_widget_settings();
+	widget.show_all();
+	return widget;
 };
--- utils.js
+++ utils.js
@@ -4,228 +4,228 @@ const Gtk = imports.gi.Gtk;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 function Utils() {
-  this._init();
+	this._init();
 }
 
 Utils.prototype = {
 
-  _filename: "",
-  _settingsObject: { },
-  _settings: "",
+	_filename: "",
+	_settingsObject: { },
+	_settings: "",
 
-  _changeEventListeners: [],
+	_changeEventListeners: [],
 
-  CENTER_WIDTH: "center-width",
-  CENTER_HEIGHT: "center-height",
-  CENTER_KEEP_WIDTH: "center-keep-width",
-  ALWAYS_USE_WIDTHS: "always-use-widths",
-  CORNER_CHANGE: "corner-changes",
-  REVERSE_MOVE_CENTER: "reverse-move-center",
+	CENTER_WIDTH: "center-width",
+	CENTER_HEIGHT: "center-height",
+	CENTER_KEEP_WIDTH: "center-keep-width",
+	ALWAYS_USE_WIDTHS: "always-use-widths",
+	CORNER_CHANGE: "corner-changes",
+	REVERSE_MOVE_CENTER: "reverse-move-center",
 
-  MOVE_FOCUS_ENABLED: "move-focus-enabled",
+	MOVE_FOCUS_ENABLED: "move-focus-enabled",
 
-  INTELLIGENT_CORNER_MOVEMENT: "intelligent-corner-movement",
+	INTELLIGENT_CORNER_MOVEMENT: "intelligent-corner-movement",
 
-  START_CONFIG: {
-    autoMove: false,
-    positions: [{
-      x: 0,
-      y: 0,
-      width: 50,
-      height: 100
-    }]
-  },
+	START_CONFIG: {
+		autoMove: false,
+		positions: [{
+			x: 0,
+			y: 0,
+			width: 50,
+			height: 100
+		}]
+	},
 
-  _init: function() {
-    this.loadSettings();
+	_init: function() {
+		this.loadSettings();
 
-    // locations is a json strings stored in the gschema. -> reload it after the user saved his changes
-    this._changeEventListeners.push({
-      name: "locations",
-      fn:  Lang.bind(this, this.loadSettings)
-    });
+		// locations is a json strings stored in the gschema. -> reload it after the user saved his changes
+		this._changeEventListeners.push({
+			name: "locations",
+			fn:  Lang.bind(this, this.loadSettings)
+		});
 
-    for (let i=0; i < this._changeEventListeners.length; i++) {
-      this._changeEventListeners[i].handlerId = this._settingsObject.connect(
-        'changed::' + this._changeEventListeners[i].name ,
-        this._changeEventListeners[i].fn
-      );
-    }
+		for (let i=0; i < this._changeEventListeners.length; i++) {
+			this._changeEventListeners[i].handlerId = this._settingsObject.connect(
+				'changed::' + this._changeEventListeners[i].name ,
+				this._changeEventListeners[i].fn
+			);
+		}
 
-  },
+	},
 
-  destroy: function() {
-    for (let i=0; i < this._changeEventListeners.length; i++) {
-      this._settingsObject.disconnect(this._changeEventListeners[i].handlerId);
-    }
-  },
+	destroy: function() {
+		for (let i=0; i < this._changeEventListeners.length; i++) {
+			this._settingsObject.disconnect(this._changeEventListeners[i].handlerId);
+		}
+	},
 
-  getSettingsObject: function() {
-    return this._settingsObject;
-  },
+	getSettingsObject: function() {
+		return this._settingsObject;
+	},
 
-  loadSettings: function() {
-    let schema = Me.metadata['settings-schema'];
+	loadSettings: function() {
+		let schema = Me.metadata['settings-schema'];
 
-    const GioSSS = Gio.SettingsSchemaSource;
+		const GioSSS = Gio.SettingsSchemaSource;
 
-    let schemaDir = Me.dir.get_child('schemas');
-    let schemaSource;
-    if (schemaDir.query_exists(null)) {
-      schemaSource = GioSSS.new_from_directory(schemaDir.get_path(), GioSSS.get_default(), false);
-    } else {
-      schemaSource = GioSSS.get_default();
-    }
+		let schemaDir = Me.dir.get_child('schemas');
+		let schemaSource;
+		if (schemaDir.query_exists(null)) {
+			schemaSource = GioSSS.new_from_directory(schemaDir.get_path(), GioSSS.get_default(), false);
+		} else {
+			schemaSource = GioSSS.get_default();
+		}
 
-    schema = schemaSource.lookup(schema, true);
-    if (!schema) {
-        throw new Error('Schema ' + schema + ' could not be found for extension '
-                        + Me.metadata.uuid + '. Please check your installation.');
-    }
+		schema = schemaSource.lookup(schema, true);
+		if (!schema) {
+			throw new Error('Schema ' + schema + ' could not be found for extension '
+				+ Me.metadata.uuid + '. Please check your installation.');
+		}
 
-    this._settingsObject = new Gio.Settings({ settings_schema: schema });
-    this._settings = {
+		this._settingsObject = new Gio.Settings({ settings_schema: schema });
+		this._settings = {
 
-    };
-  },
+		};
+	},
 
-  saveSettings: function() {
-    try {
-     
-      // sometimes the shell hangs after the gtk messagebox is displayed :(
-      // this.showMessage("Success!", "Changes successfully saved");
-    } catch (e) {
-      this.showErrorMessage("Error saving settings ", e);
-    }
-  },
-  
-  _addToArray: function(array, value) {
-    if (array.indexOf(value) == -1) {
-      array.push(value);
-    }
-    return array;
-  },
+	saveSettings: function() {
+		try {
+		 
+			// sometimes the shell hangs after the gtk messagebox is displayed :(
+			// this.showMessage("Success!", "Changes successfully saved");
+		} catch (e) {
+			this.showErrorMessage("Error saving settings ", e);
+		}
+	},
+	
+	_addToArray: function(array, value) {
+		if (array.indexOf(value) == -1) {
+			array.push(value);
+		}
+		return array;
+	},
 
-  getBoolean: function(name, defaultValue) {
-    let ret = defaultValue;
-    if (name.indexOf("locations") == -1) {
-      ret = this._settingsObject.get_int(name);
-    } else {
-      ret = this.getParameter(name);
-    }
-    return ret == "true" || ret == "1";
-  },
+	getBoolean: function(name, defaultValue) {
+		let ret = defaultValue;
+		if (name.indexOf("locations") == -1) {
+			ret = this._settingsObject.get_int(name);
+		} else {
+			ret = this.getParameter(name);
+		}
+		return ret == "true" || ret == "1";
+	},
 
-  getNumber: function(name, defaultValue) {
-    if (name.indexOf("locations") == -1) {
-      return this._settingsObject.get_int(name);
-    }
+	getNumber: function(name, defaultValue) {
+		if (name.indexOf("locations") == -1) {
+			return this._settingsObject.get_int(name);
+		}
 
-    return this._toNumber(this.getParameter(name, defaultValue), defaultValue);
-  },
+		return this._toNumber(this.getParameter(name, defaultValue), defaultValue);
+	},
 
-  get_strv: function(name) {
-    return this._settingsObject.get_strv(name);
-  },
+	get_strv: function(name) {
+		return this._settingsObject.get_strv(name);
+	},
 
-  set_strv: function(name, value) {
-    this._settingsObject.set_strv(name, value);
-  },
+	set_strv: function(name, value) {
+		this._settingsObject.set_strv(name, value);
+	},
 
-  getParameter: function(name, defaultValue) {
-    try {
-      let path = name.split("."),
-      value = this._settings[path[0]],
-      pathLength = path.length;
-      for (let i=1; i < pathLength; i++) {
-        value = value[path[i]];
-      }
+	getParameter: function(name, defaultValue) {
+		try {
+			let path = name.split("."),
+			value = this._settings[path[0]],
+			pathLength = path.length;
+			for (let i=1; i < pathLength; i++) {
+				value = value[path[i]];
+			}
 
-      return value;
-    } catch (e) {
-      this.showErrorMessage("Error getting parameter!", "Can not get config by name " + name + " defaulting to " + defaultValue + "'" + e.message);
-      return defaultValue;
-    }
-  },
+			return value;
+		} catch (e) {
+			this.showErrorMessage("Error getting parameter!", "Can not get config by name " + name + " defaulting to " + defaultValue + "'" + e.message);
+			return defaultValue;
+		}
+	},
 
-  unsetParameter: function(name) {
-    let path = name.split("."),
-      conf = this._settings[path[0]],
-      pathLength = path.length - 1;
+	unsetParameter: function(name) {
+		let path = name.split("."),
+			conf = this._settings[path[0]],
+			pathLength = path.length - 1;
 
-    for (let i=1; i < pathLength; i++) {
-      conf = conf[path[i]];
-    }
+		for (let i=1; i < pathLength; i++) {
+			conf = conf[path[i]];
+		}
 
-    if (isNaN(path[pathLength])) {
-      // normal object
-      delete conf[ path[pathLength] ];
-    } else {
-      // an array
-      conf.pop(path[pathLength]);
-    }
-  },
+		if (isNaN(path[pathLength])) {
+			// normal object
+			delete conf[ path[pathLength] ];
+		} else {
+			// an array
+			conf.pop(path[pathLength]);
+		}
+	},
 
-  setParameter: function(name, value) {
-    try {
-      if (name.indexOf("locations") == -1) {
-        if (isNaN(value)) {
-          this._settingsObject.set_string(name, value);
-        } else {
-          this._settingsObject.set_int(name, value);
-        }
-        return;
-      }
+	setParameter: function(name, value) {
+		try {
+			if (name.indexOf("locations") == -1) {
+				if (isNaN(value)) {
+					this._settingsObject.set_string(name, value);
+				} else {
+					this._settingsObject.set_int(name, value);
+				}
+				return;
+			}
 
-      let path = name.split("."),
-        conf = this._settings,
-        pathLength = path.length - 1;
+			let path = name.split("."),
+				conf = this._settings,
+				pathLength = path.length - 1;
 
-      for (let i=0; i < pathLength; i++) {
-        if (!conf[path[i]]) {
-          conf[path[i]] = {};
-        }
-        conf = conf[path[i]];
-      }
+			for (let i=0; i < pathLength; i++) {
+				if (!conf[path[i]]) {
+					conf[path[i]] = {};
+				}
+				conf = conf[path[i]];
+			}
 
-      conf[ path[pathLength] ] = value;
+			conf[ path[pathLength] ] = value;
 
-    } catch (e) {
-      this.showErrorMessage("Error setting parameter!", "Can not set config parameter " + name + " " + e.message);
-    }
-  },
+		} catch (e) {
+			this.showErrorMessage("Error setting parameter!", "Can not set config parameter " + name + " " + e.message);
+		}
+	},
 
-  _toNumber: function(value, defaultValue) {
-    let valueType = typeof(value);
+	_toNumber: function(value, defaultValue) {
+		let valueType = typeof(value);
 
-    if (valueType == "undefined") {
-      return defaultValue;
-    }
+		if (valueType == "undefined") {
+			return defaultValue;
+		}
 
-    if (isNaN(defaultValue)) {
-      defaultValue = 0;
-    }
+		if (isNaN(defaultValue)) {
+			defaultValue = 0;
+		}
 
-    return !isNaN(value)
-      ? new Number(value)
-      : defaultValue;
-  },
+		return !isNaN(value)
+			? new Number(value)
+			: defaultValue;
+	},
 
-  showMessage: function(title, message) {
-    var md = new Gtk.MessageDialog({
-      modal:true,
-      message_type: Gtk.MessageType.INFO,
-      buttons:Gtk. ButtonsType.OK,
-      title: title,
-      text: " " + message
-    });
+	showMessage: function(title, message) {
+		var md = new Gtk.MessageDialog({
+			modal:true,
+			message_type: Gtk.MessageType.INFO,
+			buttons:Gtk. ButtonsType.OK,
+			title: title,
+			text: " " + message
+		});
 
-    md.run();
-    md.destroy();
-  },
+		md.run();
+		md.destroy();
+	},
 
-  showErrorMessage: function(title, message) {
-    global.log("ERROR: " + title + " " + message);
-    //throw new Error(title + ' ' message);
-  }
+	showErrorMessage: function(title, message) {
+		global.log("ERROR: " + title + " " + message);
+		//throw new Error(title + ' ' message);
+	}
 };
